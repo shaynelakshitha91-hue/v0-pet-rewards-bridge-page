3c7964804cb7f6f11838e4a6c9c75b95528a8bae
shaynelakshitha91-hue <shaynelakshitha91@gmail.com>
2025-11-23 22:00:07 +0530

    Integrate Statcounter for analytics tracking
    Added Statcounter scripts for web analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from "next/script"
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
@@ -36,9 +37,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased`}>
+      <body className="font-sans antialiased">
         {children}
         <Analytics />
+
+        {/* Statcounter embed */}
+        <Script
+          id="statcounter-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var sc_project=13180275; 
+              var sc_invisible=1; 
+              var sc_security="6261fdb7"; 
+            `
+          }}
+        />
+
+        <Script
+          id="statcounter-lib"
+          strategy="afterInteractive"
+          src="https://www.statcounter.com/counter/counter.js"
+          async
+        />
+
+        <noscript>
+          <div className="statcounter">
+            <a 
+              title="Web Analytics"
+              href="https://statcounter.com/"
+              target="_blank"
+            >
+              <img
+                className="statcounter"
+                src="https://c.statcounter.com/13180275/0/6261fdb7/1/"
+                alt="Web Analytics"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </a>
+          </div>
+        </noscript>
+
       </body>
     </html>
   )
